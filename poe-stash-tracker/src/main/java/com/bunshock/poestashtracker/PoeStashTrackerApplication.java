d4704f57bc5ff1450eb3090432020a7f1c5babe6
Bunshock <leomanto98@gmail.com>
2026-03-23 12:56:56 -0300

    First project structure. Be sure to run a PostgreSQL db with the configured parameters (name, port, ...)

---
+++ poe-stash-tracker/src/main/java/com/bunshock/poestashtracker/PoeStashTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.bunshock.poestashtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoeStashTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoeStashTrackerApplication.class, args);
+	}
+
+}
---
+++ poe-stash-tracker/src/test/java/com/bunshock/poestashtracker/PoeStashTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunshock.poestashtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoeStashTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
